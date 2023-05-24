78086625e6559a364002823cd441db04ddc3a2d2
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-05-24 21:20:11 +0300

    add joi user validation schema

---
+++ schemas/userValidate.js
@@ -0,0 +1,22 @@
+const Joi = require("joi");
+
+const registerSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .required(),
+  password: Joi.string().min(6).required(),
+});
+
+const loginSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .required(),
+  password: Joi.string().min(6).required(),
+});
+
+module.exports = {
+  registerSchema,
+  loginSchema,
+};
