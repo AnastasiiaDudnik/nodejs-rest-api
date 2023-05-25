0abb6cfe64688cf85e62e0ec35f6e762f50d4318
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-05-25 14:57:16 +0300

    manage register controller

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,12 @@
 const express = require("express");
 
+const { register } = require("../../controllers/userController");
+
+const { registerSchema } = require("../../schemas/userValidate");
+const { validate } = require("../../decorators");
+
 const router = express.Router();
+
+router.post("/users/register", validate(registerSchema), register);
 
 module.exports = router;
