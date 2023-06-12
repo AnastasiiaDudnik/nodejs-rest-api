d84f76d0a843cd519147366380899eafe11a3643
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-06-12 15:50:23 +0300

    send email helper

--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,11 @@
 const HttpError = require("./HttpError");
 const MongooseError = require("./MongooseError");
 const imageResize = require("./imgResize");
+const sendEmail = require("./sendEmail");
 
 module.exports = {
   HttpError,
   MongooseError,
   imageResize,
+  sendEmail,
 };
